04a29d7608ed652e86fae0856595c08a3d8b4e5d
samet <sametcn99@gmail.com>
2024-05-18 04:13:25 +0300

    chore: Comment out Navbar component in RootLayout
    The Navbar component in the RootLayout file is commented out to temporarily remove it from the rendered page. This change is made to address a specific issue or requirement.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body
         className={`${montserrat.className} selection:text-text mx-auto max-w-screen-2xl bg-background-900 px-3 text-text-50 antialiased selection:bg-text-700 `}
       >
-        <Navbar />
+        {/* <Navbar /> */}
         {children}
         <Footer />
         <ScrollTop />
