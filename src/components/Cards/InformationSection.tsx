ff0dda8494ac2b0340a18f3d81cb146977eb291d
samet <sametcn99@gmail.com>
2024-05-19 01:33:32 +0300

    chore: Update InformationSection component with shadow effect
    The InformationSection component in InformationSection.tsx is updated to include a shadow effect using the "shadow-xl" class. This change is made to enhance the visual appearance of the section and provide a more immersive user experience.

--- src/components/Cards/InformationSection.tsx
+++ src/components/Cards/InformationSection.tsx
@@ -12,7 +12,7 @@ export default function InformationSection({
       className="flex min-h-screen w-full flex-row flex-wrap items-center justify-center gap-6"
       id={id}
     >
-      <div className="gradient flex w-full flex-row flex-wrap justify-center  gap-6 rounded-2xl p-6 text-text-200">
+      <div className="gradient flex w-full flex-row flex-wrap justify-center  gap-6 rounded-2xl p-6 text-text-200 shadow-xl">
         {children}
       </div>
     </section>
