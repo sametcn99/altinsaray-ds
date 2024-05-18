ba8d72357e721b5b3a2713743507be621718d06d
samet <sametcn99@gmail.com>
2024-05-19 01:19:32 +0300

    chore: Update MapCard component with responsive sizing
    The MapCard component in MapCard.tsx is updated to include responsive sizing for the iframe element. The height and width of the iframe are adjusted to be 20rem on small screens and 30rem on medium screens. This change is made to ensure the map displays properly on different devices.

--- src/components/Cards/MapCard.tsx
+++ src/components/Cards/MapCard.tsx
@@ -9,7 +9,7 @@ export default function MapCard() {
       <iframe
         title="Google Map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3056.9970445494037!2d32.797512976442555!3d39.986171381731424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34bf0f51cf46b%3A0xf9a948d87ca30b42!2sBur%C3%A7%2C%20Piri%20Cd.%2078%20C%2C%2006210%20Yenimahalle%2FAnkara!5e0!3m2!1sen!2str!4v1715987001564!5m2!1sen!2str"
-        className="rounded-2xl shadow-2xl md:h-[30rem] md:w-[30rem]"
+        className="h-[20rem] w-full rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
         id="google-map"
