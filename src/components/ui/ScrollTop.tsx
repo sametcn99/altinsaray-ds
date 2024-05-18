673fee38c9fb137018a009eddbf2a6139b435783
samet <sametcn99@gmail.com>
2024-05-19 02:50:29 +0300

    chore: Update ScrollTop component styles
    Update the styles of the ScrollTop component in ScrollTop.tsx to improve the visual appearance and user experience. The fill color of the UpArrowIcon SVG element is changed to "rgb(129 109 24 / var(--tw-text-opacity))" to match the design requirements. The className of the container div is modified to use the "bg-background-200" class and the padding is adjusted to "p-6" for better spacing. These changes ensure consistency and enhance the overall look of the ScrollTop component.

--- src/components/ui/ScrollTop.tsx
+++ src/components/ui/ScrollTop.tsx
@@ -13,7 +13,7 @@ const UpArrowIcon = () => (
   >
     <path
       d="M18.221,7.206l9.585,9.585c0.879,0.879,0.879,2.317,0,3.195l-0.8,0.801c-0.877,0.878-2.316,0.878-3.194,0  l-7.315-7.315l-7.315,7.315c-0.878,0.878-2.317,0.878-3.194,0l-0.8-0.801c-0.879-0.878-0.879-2.316,0-3.195l9.587-9.585  c0.471-0.472,1.103-0.682,1.723-0.647C17.115,6.524,17.748,6.734,18.221,7.206z"
-      fill="#515151"
+      fill="#fff"
     />
   </svg>
 );
@@ -58,7 +58,7 @@ export default function ScrollTop() {
     <>
       {visible && (
         <div
-          className={`fixed bottom-4 right-4 z-10 flex h-5 w-5 items-center justify-center rounded-lg bg-accent-500 bg-opacity-50 p-4 backdrop-blur-sm hover:shadow-xl`}
+          className={`fixed bottom-4 right-4 z-50 flex h-5 w-5 items-center justify-center rounded-lg bg-background-200 p-6 transition-all duration-300 hover:shadow-xl`}
         >
           <button
             aria-label="Scroll to Top"
