09e9782ce051a73bf04fceb5107d6423b6af7f1c
samet <sametcn99@gmail.com>
2024-05-19 05:01:36 +0300

    Update MapCard component to handle Google Map loading error

--- src/components/Cards/MapCard.tsx
+++ src/components/Cards/MapCard.tsx
@@ -1,16 +1,49 @@
+"use client";
+import { useState } from "react";
 import InformationSection from "./InformationSection";
 import DescriptionCard from "./DescriptionCard";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function MapCard() {
+  const [iframeLoaded, setIframeLoaded] = useState(true);
+
+  const handleIframeError = () => {
+    setIframeLoaded(false);
+  };
+
   return (
     <InformationSection id="ulasim">
-      <iframe
-        title="Google Map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.328947515801!2d32.84100599185166!3d39.890739990973216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f62f5fb7065%3A0xe1e5aa61fcd7c7fb!2sAltinsaray%20Dugun%20Salonu!5e0!3m2!1sen!2str!4v1716083418282!5m2!1sen!2str"
-        className="h-[20rem] w-full rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
-        referrerPolicy="no-referrer-when-downgrade"
-        id="google-map"
-      ></iframe>
+      {iframeLoaded ? (
+        <iframe
+          title="Google Map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.328947515801!2d32.84100599185166!3d39.890739990973216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f62f5fb7065%3A0xe1e5aa61fcd7c7fb!2sAltinsaray%20Dugun%20Salonu!5e0!3m2!1sen!2str!4v1716083418282!5m2!1sen!2str"
+          className="h-[20rem] w-full rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
+          referrerPolicy="no-referrer-when-downgrade"
+          id="google-map"
+          onError={handleIframeError}
+        ></iframe>
+      ) : (
+        <Link
+          className="flex h-[20rem] w-full flex-col items-center justify-center gap-2 rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
+          href="https://www.google.com/maps/place/Altinsaray%20Dugun%20Salonu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/hata.png"
+            alt="Google Map"
+            objectFit="cover"
+            className="rounded-2xl"
+            width={300}
+            height={300}
+          />
+          <p className="text-center font-bold">
+            Canlı Harita Yüklenemedi. <br />
+            Haritayı açmak için tıklayın.
+          </p>
+        </Link>
+      )}
       <DescriptionCard title="Ulaşım">
         <p>
           Piri Caddesi, No: 30, Burç Mahallesi, Ankara&apos;daki düğün
