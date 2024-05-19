63bd2aaa10961d51fe45cd11db673610ada20259
samet <sametcn99@gmail.com>
2024-05-19 03:37:37 +0300

    feat: Add website link to social media

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import { navigationLinks } from "@/lib/navigation";
 import { socialMedia } from "@/lib/socialMedia";
+import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="gradient mb-2 grid h-fit w-full grid-cols-2 rounded-2xl p-2 py-10 font-bold text-text-200 shadow-2xl">
+    <footer className="gradient relative mb-2 grid h-fit w-full grid-cols-2 rounded-2xl p-2 py-10 font-bold text-text-200 shadow-2xl">
       <div className="flex flex-col items-center">
         <div className="grid gap-2">
           {navigationLinks.map((item, index) => (
@@ -26,6 +27,13 @@ export default function Footer() {
           </a>
         ))}
       </div>
+      <Link
+        href="https://www.sametcc.me/"
+        target="_blank"
+        className="absolute bottom-2 left-1/2 -translate-x-1/2 transform text-sm text-text-100 transition-all duration-700 hover:scale-105 hover:text-text-200"
+      >
+        Tasarım
+      </Link>
     </footer>
   );
 }
