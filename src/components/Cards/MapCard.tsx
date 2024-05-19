08ef67fa028297641c3433586d6b32a3b4ad9f35
samet <sametcn99@gmail.com>
2024-05-19 04:50:39 +0300

    Update Google Map location in MapCard component

--- src/components/Cards/MapCard.tsx
+++ src/components/Cards/MapCard.tsx
@@ -6,9 +6,8 @@ export default function MapCard() {
     <InformationSection id="ulasim">
       <iframe
         title="Google Map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3056.9970445494037!2d32.797512976442555!3d39.986171381731424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34bf0f51cf46b%3A0xf9a948d87ca30b42!2sBur%C3%A7%2C%20Piri%20Cd.%2078%20C%2C%2006210%20Yenimahalle%2FAnkara!5e0!3m2!1sen!2str!4v1715987001564!5m2!1sen!2str"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.328947515801!2d32.84100599185166!3d39.890739990973216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f62f5fb7065%3A0xe1e5aa61fcd7c7fb!2sAltinsaray%20Dugun%20Salonu!5e0!3m2!1sen!2str!4v1716083418282!5m2!1sen!2str"
         className="h-[20rem] w-full rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
-        allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
         id="google-map"
       ></iframe>
