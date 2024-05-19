fab423e37dba18de3e7e052986af4fd6facc9078
samet <sametcn99@gmail.com>
2024-05-19 05:18:57 +0300

    feat: Lazy load Google Map iframe in MapCard component

--- src/components/Cards/MapCard.tsx
+++ src/components/Cards/MapCard.tsx
@@ -4,9 +4,10 @@ import InformationSection from "./InformationSection";
 import DescriptionCard from "./DescriptionCard";
 import Link from "next/link";
 import Image from "next/image";
+import { isMobile } from "react-device-detect";
 
 export default function MapCard() {
-  const [iframeLoaded, setIframeLoaded] = useState(false);
+  const [iframeLoaded, setIframeLoaded] = useState(true);
 
   const handleIframeError = () => {
     setIframeLoaded(false);
@@ -14,17 +15,7 @@ export default function MapCard() {
 
   return (
     <InformationSection id="ulasim">
-      {iframeLoaded ? (
-        <iframe
-          title="Google Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.328947515801!2d32.84100599185166!3d39.890739990973216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f62f5fb7065%3A0xe1e5aa61fcd7c7fb!2sAltinsaray%20Dugun%20Salonu!5e0!3m2!1sen!2str!4v1716083418282!5m2!1sen!2str"
-          className="h-[20rem] w-full rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
-          referrerPolicy="no-referrer-when-downgrade"
-          id="google-map"
-          onError={handleIframeError}
-          sandbox="allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation"
-        ></iframe>
-      ) : (
+      {isMobile ? (
         <Link
           className="flex h-[20rem] w-full flex-col items-center justify-center gap-2 rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
           href="https://www.google.com/maps/place/Altinsaray%20Dugun%20Salonu"
@@ -36,15 +27,49 @@ export default function MapCard() {
             alt="Google Map"
             objectFit="cover"
             className="rounded-2xl"
-            width={130}
-            height={130}
+            width={150}
+            height={150}
           />
           <p className="text-center font-bold">
             Canlı Harita Yüklenemedi. <br />
             Haritayı açmak için tıklayın.
           </p>
         </Link>
+      ) : (
+        <>
+          {iframeLoaded ? (
+            <iframe
+              title="Google Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d191.328947515801!2d32.84100599185166!3d39.890739990973216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f62f5fb7065%3A0xe1e5aa61fcd7c7fb!2sAltinsaray%20Dugun%20Salonu!5e0!3m2!1sen!2str!4v1716083418282!5m2!1sen!2str"
+              className="h-[20rem] w-full rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
+              referrerPolicy="no-referrer-when-downgrade"
+              id="google-map"
+              onError={handleIframeError}
+            ></iframe>
+          ) : (
+            <Link
+              className="flex h-[20rem] w-full flex-col items-center justify-center gap-2 rounded-2xl shadow-2xl sm:h-[20rem] sm:w-[20rem] md:h-[30rem] md:w-[30rem]"
+              href="https://www.google.com/maps/place/Altinsaray%20Dugun%20Salonu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/hata.png"
+                alt="Google Map"
+                objectFit="cover"
+                className="rounded-2xl"
+                width={150}
+                height={150}
+              />
+              <p className="text-center font-bold">
+                Canlı Harita Yüklenemedi. <br />
+                Haritayı açmak için tıklayın.
+              </p>
+            </Link>
+          )}
+        </>
       )}
+
       <DescriptionCard title="Ulaşım">
         <p>
           Piri Caddesi, No: 30, Burç Mahallesi, Ankara&apos;daki düğün
