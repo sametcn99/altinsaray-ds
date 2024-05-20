0507a29fc353277ec7622fe82de550a484ec8fb3
samet <sametcn99@gmail.com>
2024-05-21 00:58:36 +0300

    Update Hero component layout to fix flex-wrap direction and improve section styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -12,7 +12,9 @@ export default function Hero() {
       id="hosgeldiniz"
     >
       <div className="flex  flex-col flex-wrap gap-2 pt-4">
-        <p className="max-w-[50rem] text-4xl font-light">{texts.catchword}</p>
+        <p className="max-w-[50rem] text-center text-4xl font-light md:text-start">
+          {texts.catchword}
+        </p>
         <h1 className="gradient bg-clip-text text-center text-5xl font-black text-transparent sm:text-8xl">
           Altın Saray
           <br />
--- src/components/Sections/Navbar/Navbar.tsx
+++ src/components/Sections/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { navigationLinks } from "@/lib/navigation";
 import Link from "next/link";
-import { FaPhone } from "react-icons/fa6";
+import { FaPhone, FaBars, FaTimes } from "react-icons/fa";
 import { Courgette } from "next/font/google";
 import { cn } from "@/lib/utils";
 
@@ -13,34 +13,50 @@ const font = Courgette({
 
 export default function Navbar() {
   const [hover, setHover] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleClick = (id: string) => {
     const element = document.getElementById(id);
     element?.scrollIntoView({ behavior: "smooth" });
+    setMenuOpen(false); // Close menu after clicking
   };
 
   return (
     <nav
       id="navbar"
       className={cn(
-        `z-50 flex w-full select-none flex-row flex-wrap items-center justify-center gap-2 text-nowrap rounded-2xl px-4 text-2xl lg:justify-between`,
+        `z-50 flex w-full select-none flex-row flex-wrap items-center justify-between gap-2 text-nowrap rounded-2xl px-4 text-2xl lg:justify-between`,
       )}
     >
       <Link
         href="/"
-        className={`${font.className} gradient flex select-none bg-clip-text p-4 text-4xl`}
+        className={`${font.className} gradient flex select-none bg-clip-text p-4 text-3xl`}
         onDragStart={(e) => e.preventDefault()}
       >
         Altın Saray
         <br />
         Wedding Hall
       </Link>
-      <div className={cn(` flex min-w-64 flex-row justify-center gap-2`)}>
-        <div className={cn(`flex flex-row flex-wrap justify-center gap-2`)}>
+      <div className="lg:hidden">
+        <button className="text-3xl" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+      <div
+        className={cn(
+          `fixed right-7 top-16 z-50 mt-12 w-[16rem] flex-col items-center justify-between gap-2 rounded-2xl bg-background-300 bg-opacity-70 p-2 backdrop-blur-sm lg:static lg:flex lg:w-auto lg:flex-row lg:bg-transparent`,
+          {
+            hidden: !menuOpen,
+            flex: menuOpen,
+          },
+          ``,
+        )}
+      >
+        <div className={cn(`flex flex-col items-center gap-2 lg:flex-row`)}>
           {navigationLinks.map((link) => (
             <button
               key={link.href}
-              className="transition-all duration-700 hover:scale-105 hover:font-bold"
+              className="transition-all duration-1000 hover:scale-105 hover:font-bold"
               onDragStart={(e: any) => e.preventDefault()}
               onClick={() => handleClick(link.id)}
             >
@@ -48,22 +64,41 @@ export default function Navbar() {
             </button>
           ))}
         </div>
+        {menuOpen && (
+          <Link
+            href={"tel:+905555555555"}
+            className="flex flex-row items-center gap-2 transition-all duration-700 hover:font-medium lg:hidden"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onDragStart={(e) => e.preventDefault()}
+          >
+            <FaPhone
+              className={cn(
+                `m-2 inline -scale-x-90 transform rounded-2xl  text-4xl transition-all duration-500`,
+                hover ? "rotate-6 font-bold text-green-500" : "",
+              )}
+            />
+            <div className={cn("flex flex-col")}>
+              <span>0555 555 55 55</span>
+              <span className="text-sm">Bize Ulaşın</span>
+            </div>
+          </Link>
+        )}
       </div>
       <Link
         href={"tel:+905555555555"}
-        className="flex flex-row items-center gap-2 transition-all duration-700 hover:font-medium"
+        className="hidden flex-row items-center gap-2 transition-all duration-700 hover:font-medium lg:flex"
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
         onDragStart={(e) => e.preventDefault()}
       >
-        <div>
-          <FaPhone
-            className={cn(
-              `m-2 inline rounded-2xl p-2 text-5xl transition-all duration-500 ${hover ? "rotate-12 font-bold text-green-500" : "rotate-0"}`,
-            )}
-          />
-        </div>
-        <div className="flex flex-col">
+        <FaPhone
+          className={cn(
+            `m-2 inline -scale-x-90 transform rounded-2xl  text-4xl transition-all duration-500`,
+            hover ? "rotate-6 font-bold text-green-500" : "",
+          )}
+        />
+        <div className={cn("flex flex-col")}>
           <span>0555 555 55 55</span>
           <span className="text-sm">Bize Ulaşın</span>
         </div>
